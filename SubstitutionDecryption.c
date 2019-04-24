1fc67a6ffb0dea6de2898353208738acfb9bca67
3331585 <49224940+3331585@users.noreply.github.com>
2019-04-24 21:14:37 +1000

    Update SubstitutionDecryption.c

--- SubstitutionDecryption.c
+++ SubstitutionDecryption.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-//
 
 int main()
 {
-
+    char messageletter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char ciphertextletter[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    char input[] = "PQDOT";
+    int n = 5;
+    int index = 0;
+    int counter = 0;
+    
+    while (index <= n){
+        for (counter = 0; counter < 25; counter ++){
+            if (input[index] == ciphertextletter[counter]){
+            printf("%c", messageletter[counter]);
+            }
+        }
+        index ++;
+    }
+    
 return 0;
 }
